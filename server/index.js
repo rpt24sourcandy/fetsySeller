220611defed04dca74fbb18bc12b9fd8ca7e3e34
Kendall Butt <kendallbutt1@gmail.com>
2020-11-16 20:55:59 -0800

    add else block to get request

--- server/index.js
+++ server/index.js
@@ -12,15 +12,14 @@ app.use(express.urlencoded({ extended: false }))
 
 app.get('/seller/info/', (req, res) => {
   pool.query('SELECT * FROM seller_info', (err, data) => {
-    if (err) console.log(err)
-    res.status(200).send(data)
+    if (err) {
+      console.log(err)
+    } else {
+      res.status(200).send(data)
+    }
   })
 })
 
-// app.put('/', (req, res) => {
-//   let query = 'INSERT INTO seller_info (seller_rating, total_sales, seller_name, seller_city, seller_country, on_etsy_since) VALUES ()'
-//   pool.query()
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
