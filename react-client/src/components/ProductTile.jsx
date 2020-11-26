62c0e1f5cb9cbc23ef7bd37e23474206e8cc66d2
Kendall Butt <kendallbutt1@gmail.com>
2020-11-25 18:51:22 -0800

    CSS and GET image data

--- react-client/src/components/ProductTile.jsx
+++ react-client/src/components/ProductTile.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const DIV = styled.div`
+  font-family: "Guardian-EgypTT",serif;
+  font - weight: 300;
+  font - size: 13px;
+  line - height: 18px;
+  text-overflow: elipsis !important;
+  overflow: hidden !important;
+  white-space: nowrap !important;
+  min-width: 0 !important;
+  color: rgb(34, 34, 34);
+`
+const H5 = styled.h5`
+  font-family: "Graphik Webfont",-apple-system,BlinkMacSystemFont,"Roboto","Droid Sans","Segoe UI","Helvetica",Arial,sans-serif;
+  font - weight: 500;
+  font - size: 16px;
+  line - height: 20px;
+`
+const IMG = styled.img`
+  width: 100px;
+  height: 100px;
+`
 
 function ProductTile(props) {
   let shipping;
@@ -8,11 +31,11 @@ function ProductTile(props) {
   }
   return (
     <div>
-      <img src={props.image}></img>
-      <div>{props.item.item_name}</div>
-      <div>{props.seller.seller_name}</div>
-      <h5>{props.item.price}</h5>
-      <div>{shipping}</div>
+      <IMG src={props.image}></IMG>
+      <DIV>{props.item.item_name}</DIV>
+      <DIV>{props.seller.seller_name}</DIV>
+      <H5>${props.item.price}</H5>
+      <DIV>{shipping}</DIV>
     </div>
   )
 }
--- react-client/src/components/Seller.jsx
+++ react-client/src/components/Seller.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const H2 = styled.h2`
+  font-family: "Guardian-EgypTT",serif;
+  font-weight: 300;
+  letter-spacing: 0.35px;
+  font-size: 20px;
+  color: #595959 !important;
+`
+
+const H3 = styled.h3`
+  font-family: "Guardian-EgypTT",serif;
+  font-weight: 300;
+  letter-spacing: 0.35px;
+  font-size: 16px;
+  color: #595959 !important;
+`
+
+const DIV = styled.div`
+  font-family: "Guardian-EgypTT",serif;
+  font-size: 16px;
+  color: #595959 !important;
+  font-weight: 700;
+  line-height: 24px;
+`
 
 
 function Seller(props) {
   return (
-    <div>
-      <div>Meet your sellers</div>
-      <h2>Shop Owner</h2>
-      <div>Owner of {props.seller.seller_name}</div>
-      <h3>Message {props.seller.seller_name}</h3>
-      <div>This seller usually responds within a few hours.</div>
-    </div>
+    <DIV>
+      <DIV>Meet your sellers</DIV>
+      <H2>Shop Owner</H2>
+      <DIV>Owner of {props.seller.seller_name}</DIV>
+      <H3>Message {props.seller.seller_name}</H3>
+      <DIV>This seller usually responds within a few hours.</DIV>
+    </DIV>
   )
 }
 
